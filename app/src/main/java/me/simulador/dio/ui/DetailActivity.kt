70fb4a94af092b62b6fceac1f6da07c7d09b7d1d
Cristianoalves226 <c.alvesdecampos@gmail.com>
2022-02-02 17:04:04 -0400

    Finalização do APP de simulação de partidas

---
+++ app/src/main/java/me/simulador/dio/ui/DetailActivity.kt
@@ -0,0 +1,58 @@
+package me.simulador.dio.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import me.simulador.dio.databinding.ActivityDetailBinding
+import me.simulador.dio.domain.Match
+
+class DetailActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityDetailBinding
+
+    object Extras {
+        const val MATCH = "EXTRA_MATCH"
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        loadMatchesFromExtra()
+    }
+
+    private fun loadMatchesFromExtra() {
+
+        intent?.extras?.getParcelable<Match>(Extras.MATCH)?.let {
+            Glide.with(this).load(it.place.image).into(binding.ivPlace)
+            supportActionBar?.title = it.place.name
+
+
+            binding.tvDescription.text = it.description
+
+            Glide.with(this).load(it.place.image).into(binding.ivHomeTeam)
+            binding.tvAwayTeamName.text = it.homeTeam.name
+            binding.rbHomeTeamStars.rating = it.homeTeam.starts.toFloat()
+            if (it.homeTeam.score != null) {
+                binding.tvHomeTeamScore.text = it.homeTeam.score.toString()
+            }
+
+            Glide.with(this).load(it.place.image).into(binding.ivAwayTeam)
+            binding.tvAwayTeamName.text = it.awayTeam.name
+            binding.rbAwayTeamStars.rating = it.awayTeam.starts.toFloat()
+            if (it.awayTeam.score != null) {
+                binding.tvAwayTeamScore.text = it.awayTeam.score.toString()
+            }
+
+        }
+
+    }
+}
